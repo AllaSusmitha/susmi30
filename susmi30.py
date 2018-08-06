a216de5efb9b30f87511a5a04a0214743956b83e
AllaSusmitha <41768199+AllaSusmitha@users.noreply.github.com>
2018-08-06 15:51:08 +0530

    Create susmi30.py

---
+++ susmi30.py
@@ -0,0 +1,12 @@
+N,Q=map(int,raw_input().split())
+for n in range(N,  Q):
+   order = len(str(n))
+   sum = 0
+   temp = n
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if n == sum:
+       print n,
